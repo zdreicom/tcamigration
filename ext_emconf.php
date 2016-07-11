3a870797634d988fd80ae8065bd943aff369a943
Philipp Gampe <dev@philippgampe.info>
2016-07-11 13:51:29 +0200

    [TASK] Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'TCA migration',
+    'description' => 'A helper to migrate TCA files.',
+    'category' => 'be',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Philipp Gampe ',
+    'author_email' => 'dev@philippgampe.info',
+    'author_company' => 'zdreicom',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+);
