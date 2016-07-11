b5485dc4ae3d71e535b8645eef656b7ee2b74057
Philipp Gampe <dev@philippgampe.info>
2016-07-11 15:00:11 +0200

    [TASK] Step two: scan and reevaluate TCA files

--- Classes/Command/TcaMigrationCommandController.php
+++ Classes/Command/TcaMigrationCommandController.php
@@ -14,6 +14,8 @@ namespace TYPO3\tcamigration\Command;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Package\Package;
+use TYPO3\CMS\Core\Package\PackageManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
@@ -22,6 +24,7 @@ use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
  */
 class TcaMigrationCommandController extends CommandController
 {
+
     /**
      * @var bool
      */
@@ -50,7 +53,7 @@ class TcaMigrationCommandController extends CommandController
      * @param string $tables Comma-separated list of tables to migrate
      * @cli
      */
-    public function migrateTable($extension, $tables)
+    public function migrateTableCommand($extension, $tables)
     {
         $tables = GeneralUtility::trimExplode(',', $tables);
         foreach ($tables as $table) {
@@ -58,7 +61,8 @@ class TcaMigrationCommandController extends CommandController
             $this->unsetTcaForTable($table);
         }
 
-        
+        $this->scanAndEvaluateExtensionTcaFiles($extension, $tables);
+
     }
 
     /**
@@ -66,7 +70,8 @@ class TcaMigrationCommandController extends CommandController
      *
      * @param string $table The table to backup
      */
-    protected function backupTcaForTable($table) {
+    protected function backupTcaForTable($table)
+    {
         $this->tcaBackup[$table] = $GLOBALS['TCA'][$table];
     }
 
@@ -75,8 +80,106 @@ class TcaMigrationCommandController extends CommandController
      *
      * @param string $table The table for which to unset the TCA
      */
-    protected function unsetTcaForTable($table) {
+    protected function unsetTcaForTable($table)
+    {
         unset($GLOBALS['TCA'][$table]);
+    }
+
+    /**
+     * Scans the extension for TCA related files and reevaluates them
+     *
+     * @param string $extension The name of the extension
+     * @param array $tables The array of tables to consider
+     */
+    protected function scanAndEvaluateExtensionTcaFiles($extension, array $tables)
+    {
+        $this->loadBaseTcaOfExtension($extension, $tables);
+        $this->loadExtTablesOfExtension($extension, $tables);
+    }
+
+    /**
+     * Loads the base TCA of an extension
+     *
+     * This is a copy from the core (bootstrap), limited to one extension
+     * and the given tables.
+     *
+     * @param string $extension The name of the extension
+     * @param array $tables The array of tables to consider
+     */
+    protected function loadBaseTcaOfExtension($extension, array $tables)
+    {
+        /** @var Package[] $activePackages */
+        $activePackages = GeneralUtility::makeInstance(PackageManager::class)->getActivePackages();
+        // Limit to single extension
+        $package = $activePackages[$extension];
+
+        // First load "full table" files from Configuration/TCA
+        $tcaConfigurationDirectory = $package->getPackagePath() . 'Configuration/TCA';
+        if (is_dir($tcaConfigurationDirectory)) {
+            $files = scandir($tcaConfigurationDirectory);
+            foreach ($files as $file) {
+                if (
+                    is_file($tcaConfigurationDirectory . '/' . $file)
+                    && ($file !== '.')
+                    && ($file !== '..')
+                    && (substr($file, -4, 4) === '.php')
+                ) {
+                    $tcaOfTable = require($tcaConfigurationDirectory . '/' . $file);
+                    if (is_array($tcaOfTable)) {
+                        // TCA table name is filename without .php suffix, eg 'sys_notes', not 'sys_notes.php'
+                        $tcaTableName = substr($file, 0, -4);
+                        // Limit to given tables
+                        if (in_array($tcaTableName, $tables)) {
+                            $GLOBALS['TCA'][$tcaTableName] = $tcaOfTable;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Loads the ext_tables.php file from an extension and any additional files
+     *
+     * This is a copy from the core (bootstrap), limited to one extension
+     * and the given tables.
+     *
+     * @param string $extension The name of the extension
+     * @param array $tables The array of tables to consider
+     */
+    protected function loadExtTablesOfExtension($extension, array $tables) {
+        $_EXTKEY = $extension;
+        global $_EXTKEY;
+
+        global $T3_SERVICES, $T3_VAR, $TYPO3_CONF_VARS;
+        global $TBE_MODULES, $TBE_MODULES_EXT, $TCA;
+        global $PAGES_TYPES, $TBE_STYLES;
+
+        $extensionInformation = $GLOBALS['TYPO3_LOADED_EXT'][$extension];
+        // Load each ext_tables.php file of loaded extensions
+        if ((is_array($extensionInformation) || $extensionInformation instanceof \ArrayAccess) && $extensionInformation['ext_tables.php']) {
+            // $_EXTKEY and $_EXTCONF are available in ext_tables.php
+            // and are explicitly set in cached file as well
+            $_EXTCONF = $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY];
+            require $extensionInformation['ext_tables.php'];
+
+            foreach ($tables as $tableName) {
+                if (!isset($TCA[$tableName]['columns'])) {
+                    $columnsConfigFile = $TCA[$tableName]['ctrl']['dynamicConfigFile'];
+                    if ($columnsConfigFile) {
+                        GeneralUtility::logDeprecatedFunction();
+                        if (GeneralUtility::isAbsPath($columnsConfigFile)) {
+                            include($columnsConfigFile);
+                        } else {
+                            throw new \RuntimeException(
+                                'Columns configuration file not found',
+                                1468241963
+                            );
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
